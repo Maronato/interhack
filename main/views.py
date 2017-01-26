4e0bf2247573a2fa1c25cb0e59b05f64e3dfe326
Maronato <gustavomaronato@gmail.com>
2017-01-26 17:31:27 -0200

    fixed referee requirement

--- main/views.py
+++ main/views.py
@@ -4,5 +4,7 @@ from .forms import PreRegistrationForm
 
 
 def index(request, referee_id="-1"):
+    if not referee_id:
+        referee_id = '-1'
     form = PreRegistrationForm(initial={'referee': referee_id})
     return render(request, 'main/index.html', {'form': form})
