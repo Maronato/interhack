e07f3d2cf95064f214c82c7c065b3fbd5957c1a4
Maronato <gustavomaronato@gmail.com>
2017-02-23 12:06:39 -0300

    added redirect to url

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 import main.views
 import registration.views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^(referral/(?P<referee_id>\w+)/)?$', main.views.index, name='index'),
     url(r'^registration/$', registration.views.pre_registration, name='pre-reg'),
     url(r'^\.well-known/acme-challenge/hI7WcSft0kG-Qr4r52TLnHr7atSkmStg3qRXATYCJQI', TemplateView.as_view(template_name="main/ssl.html")),
+    url(r'^MoreInfo/$', RedirectView.as_view(url="http://agency.bemyapp.com/insights/infographics-hackathon-figures-in-2016.html")),
 ]
